039cb07c9b605d956574622fb3f5a01c195a39a4
Pierre De Wit <DeWitP@users.noreply.github.com>
2016-07-08 09:03:25 +0200

    Update vcf2smartpca.py

--- scripts/vcf2smartpca.py
+++ scripts/vcf2smartpca.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
 import sys
 
-#################################################################################################
-#                                                                                               #
-#   This program creates all necc. input files for smartpca analysis                            #
-#   Usage: vcf2smartpca.py in.vcf prefix min_gen_quality_threshold                              #
-#   min_gen_quality_threshold is for the individual genotypes, not the SNP as a whole           #
-#   Author: Jason Ladner                                                                        #
-#                                                                                               #
-#################################################################################################
+#########################################################################################################################################
+#                                                                                                                                       #
+#   Written by Jason Ladner                                                                                                             #
+#   questions?: jtladner@gmail.com                                                                                                      #
+#   Usage:                                                                                                                              #
+#   python vcf2smartpca.py   input_vcf_file   prefix   genotype_quality_cutoff  popfile                                                 #
+#                                                                                                                                       #                                                                                                                                       #
+#   input_vcf_file = the .vcf file that you want to extract genotype information from                                                   #
+#   prefix = an arbitrary string of characters that will be added to the output filenames                                              #
+#   genotype_quality_cutoff = a floating point number that specifies the minimum quality allowed by genotype information to be used     #
+#   popfile = a plain text file with two columns per line, separated by some amount of whitespace                                       #
+#             The 1st column is the individual ID (exactly as in the vcf file), the 2nd is the population that the individual belongs to#
+#                                                                                                                                       #
+#########################################################################################################################################
 
 '''---------------------------> VCF Format <-----------------------------
     cols[0] = contig name
@@ -46,12 +52,12 @@ def file_dict(file):
 #        -Each indiv list will contain genotpe data for each SNP                  #
 #    -'pops' is a dictionary with indiv names as the keys and their population    #
 #      of origin as the values                                                    #
+#    - 'contigs' is a list of all the contig names for the snps being output      #
 #---------------------------------------------------------------------------------#
-def genos4smart(headers, snp_gens, pops):
+def genos4smart(headers, snp_gens, pops, contigs, pos):
     loci_names=[]
     for index, contig in enumerate(contigs):
         loci_names.append(contig + '_' + pos[index])
-
     lines=[]
     indivcount=0
     for indiv in headers[4:]:
@@ -69,9 +75,9 @@ def genos4smart(headers, snp_gens, pops):
 #     -'lines' is a list that contains an entry for each indivdual                #
 #      -Each entry is also a list, with an entry for the indiv's ID & population  #
 #      as well as two entries for each locus saying the # of each allele          #
-#                                                                                 #
+#      'omit' is an optional parameter                                            #
 #---------------------------------------------------------------------------------#
-def smartpca_input(loci_names, lines, prefix, omit=[]):
+def smartpca_input(loci_names, lines, prefix, omit=[]): 
 #Opens an output text files with prefix as specified by user
     GENO = open(prefix + '_Geno', 'w')
     INDIV = open(prefix + '_Indiv', 'w')
@@ -79,13 +85,11 @@ def smartpca_input(loci_names, lines, prefix, omit=[]):
     print "omitting: " + str(omit)
     pops=[]
     names=[]
-    
     print '# of loci = %d' % (len(loci_names))
     
     loci=[]
     for locus in loci_names:
         loci.append([])
-    
     for indiv in lines:
         
         if indiv[0] in omit: continue
@@ -121,81 +125,100 @@ def smartpca_input(loci_names, lines, prefix, omit=[]):
     SNP.close()
     INDIV.close()
 
+#---------------------------------------------------------------------------------------------------------------
+#-------> This function will create a dictionary relating your individuals to their population of origin
+#-------> Popfile should be a plain text file with two columns per line, separated by some amount of whitespace
+#-------> The first column is the individual ID (exactly as it is in the .vcf file
+#-------> The second column is the population that the individual belongs to
+#---------------------------------------------------------------------------------------------------------------
+def make_pop_dict(popfile):
+    pop_dict = {}
+    fin = open(popfile, 'r')
+    for line in fin:
+        cols = line.strip().split()
+        pop_dict[cols[0]] = cols[1]
+    return pop_dict
 
-#-------> The Actual Program Starts Here <-------
+#-------------------------------------------------------------#
+#  This function takes the genotype description for a SNP     #
+#   (e.g., GT:AD:DP:GQ:PL)                                    #
+#  and returns the position of the genotype quality (GQ)      #
+#  with the first position being 0                            #
+#-------------------------------------------------------------#
+def get_GQ_pos(genotype_description):            
+    return genotype_description.split(':').index('GQ')
 
-pops={'FR32_ATCACG_before': '1', 'FR33_TTAGGC_before': '1', 'FR34_ACTTGA_before': '1', 'FR35_GATCAG_before': '1', 'FR36_TAGCTT_before': '1', 'FR37_GGCTAC_before': '1', 'FR77_ATCACG_after': '2', 'FR78_TTAGGC_after': '2', 'FR79_ACTTGA_after': '2', 'FR80_GATCAG_after': '2', 'FR81_TAGCTT_after': '2', 'FR82_GGCTAC_after': '2'}
-
-#Opens an infile specified by the user. Should be a .vcf file 
-IN = open(sys.argv[1], 'r')
-quality_cutoff=float(sys.argv[3])                    #for individual genotypes (not the SNP as a whole)
-
-linecount=0
-contigs=[]
-pos=[]
-ref=[]
-alt=[]
-all_gens=[]
-locus=[]
-headers=[]
-
-#Reads through the vcf file line by line
-for line in IN:
-    line=line.rstrip()
-    cols=line.split('\t')
-    gens=[]                           #Will hold individual level genotype data, resets list form previous SNP
-    gencount=0                        #Will be used below to keep track of the individuals when appending new SNPs
+#-----------------------------------------------------------------------------------------------#
+#  This function takes a .vcf file and extract genotype info                                    #
+#  As inputs it requires a .vcf file and an integer to be used as a genotype quality cutoff     #
+#  It returns two variables:                                                                    #
+#  'headers' is a list of the headers of interest (i.e., those to be output)                    #
+#  'all_gens'is a list of lists with one sublist for each indiv. containing geno info           #
+#  'contigs' is a list of all the contig names for the snps being output                        #
+#-----------------------------------------------------------------------------------------------#
+def extract_geno_info(input_vcf, genotype_quality_cutoff):
+    #Opens an infile specified by the user. Should be a .vcf file 
+    IN = open(input_vcf, 'r')
+    quality_cutoff=float(genotype_quality_cutoff)                    #for individual genotypes (not the SNP as a whole)
     
-    if cols[0][0] == '#' and cols[0][1] != '#':       #To pull out headers
-
-        headers=cols[0:2] + cols[3:5] + cols[9:]      #Creates list of headers of interest
-        print headers[4:]                             #Prints the IDs of the individuals with genotypes in the vcf
-
-    if cols[0][0] != '#':                             #Specifies only SNP lines
-        linecount+=1                                  #Keeps track of the numbers of SNPs
+    linecount=0
+    all_gens=[]
+    locus=[]
+    headers=[]
+    pos=[]
+    contigs=[]
+    #Reads through the vcf file line by line
+    for line in IN:
+        line=line.rstrip()
+        cols=line.split('\t')
+        gens=[]                           #Will hold individual level genotype data, resets list form previous SNP
+        gencount=0                        #Will be used below to keep track of the individuals when appending new SNPs
         
-        gens=cols[9:]                  #Creates a list of all the genotype info
-        desc=cols[8]
-        parts=desc.split(':')			#Finds which value in the genotype info is Genotype quality
-        count=0							#and stores the position of the GQ.
-        for items in parts:
-        	if items == 'GQ':
-        		GQ_pos = count
-        	count+=1        
-        
-        if linecount==1:                      #will only execute these commands once, when reading the first line of the file
-            for gen in gens:
-                all_gens.append([])           #Creates seperate list entry for each individual, will hold genotypes at all SNPs
-                locus.append([])              #Creates seperate list entry for each individual, will hold genoypes at a specific locus 
-        
-        for gen in gens:                    #Steps through the genotype info for each individual
-            parts=gen.split(':')            #Breaks apart the different sections of the genotype info
-            genotype=parts[0]               #Most probable genotype for the individual, with the two alleles seperated by '/'
-            genotypes=genotype.split('/')
+        if cols[0][0] == '#' and cols[0][1] != '#':       #To pull out headers
+    
+            headers=cols[0:2] + cols[3:5] + cols[9:]      #Creates list of headers of interest
+            print headers[4:]                             #Prints the IDs of the individuals with genotypes in the vcf
+    
+        if cols[0][0] != '#':                             #Specifies only SNP lines
+            linecount+=1                                  #Keeps track of the numbers of SNPs
             
-            if genotypes != ['.','.']:         #Had to be added because in new version of GATK, .vcf files are written so that if there is no genotype info for an individual, their genotype is './.' as opposed to './.:0:0:0:0', or whatever
-                gen_quality=float(parts[GQ_pos])
+            gens=cols[9:]                  #Creates a list of all the genotype info
+            desc=cols[8]
+            GQ_pos = get_GQ_pos(desc)               #Pulls out the position of the genotype quality
+            
+            if linecount==1:                      #will only execute these commands once, when reading the first line of the file
+                for gen in gens:
+                    all_gens.append([])           #Creates seperate list entry for each individual, will hold genotypes at all SNPs
+                    locus.append([])              #Creates seperate list entry for each individual, will hold genoypes at a specific locus 
+            
+            for gen in gens:                    #Steps through the genotype info for each individual
+                parts=gen.split(':')            #Breaks apart the different sections of the genotype info
+                genotype=parts[0]               #Most probable genotype for the individual, with the two alleles seperated by '/'
+                genotypes=genotype.split('/')
                 
-                if gen_quality<quality_cutoff:
-                    genotypes=['.','.']
-                
-            locus[gencount]=genotypes           #Replace genotype data from last locus with genotype form this locus
-            gencount+=1                         #Keeping track of the individual whose data is being examined
-    
-        if ['.', '.'] not in locus and locus != []:
-            ##########################################################################
-            #   Use these 4 lines to only output info for SNPs with no missing data  #
-            ##########################################################################
-            contigs.append(cols[0])   #Creates a list of all the contig names               
-            pos.append(cols[1])       #Creates a list of all the SNP base positions
-            ref.append(cols[3])       #Creates a list of all the reference bases
-            alt.append(cols[4])       #Creates a list of all the alternative bases
-            locuscount=0
-            for each in locus:
-                all_gens[locuscount].append(each)                  #Appends genotype data to the all_gens list
-                locuscount+=1                                      #keeps track of the number of loci with no missing data
+                if genotypes != ['.','.']:         #Had to be added because in new version of GATK, .vcf files are written so that if there is no genotype info for an individual, their genotype is './.' as opposed to './.:0:0:0:0', or whatever
+                    gen_quality=float(parts[GQ_pos])
+                    
+                    if gen_quality<quality_cutoff:
+                        genotypes=['.','.']
+                    
+                locus[gencount]=genotypes           #Replace genotype data from last locus with genotype form this locus
+                gencount+=1                         #Keeping track of the individual whose data is being examined
+        
+            if ['.', '.'] not in locus and locus != []:
+                contigs.append(cols[0])
+                pos.append(cols[1])
+                i_count=0
+                for each in locus:
+                    all_gens[i_count].append(each)                  #Appends genotype data to the all_gens list
+                    i_count+=1                                      #keeps track of the number of loci with no missing data
 
-print "Number of SNPs in reference contigs = %d" % (linecount)
+    print "Number of SNPs in reference contigs = %d" % (linecount)
+    return headers, all_gens, contigs, pos
 
-list_headers, list_lines = genos4smart(headers, all_gens, pops)
-smartpca_input(list_headers, list_lines, sys.argv[2])
+#-------> The Actual Script Execution Starts Here <-------
+
+headers, all_gens, contigs, pos = extract_geno_info(sys.argv[1], sys.argv[3])
+list_headers, list_lines = genos4smart(headers, all_gens, make_pop_dict(sys.argv[4]), contigs, pos)
+if len(sys.argv) == 5: smartpca_input(list_headers, list_lines, sys.argv[2])
+elif len(sys.argv) > 5: smartpca_input(list_headers, list_lines, sys.argv[2], sys.argv[5:])
